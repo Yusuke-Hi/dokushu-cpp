971c73903eb6b0b8a15c5f4b7e801a7d2b6165bd
yusuke <yuusuke29413@gmail.com>
2025-02-19 09:02:17 +0900

    class

---
+++ ch03/practice/3_5/main.cpp
@@ -0,0 +1,24 @@
+#include <string>
+class Base{
+  public:
+    virtual std::string name() const;
+    virtual std::string most_name() const = 0;
+};
+std::string Base::name() const{return "Base";}
+
+class Derived:public Base{
+  public:
+    virtual std::string name() const override;
+};
+std::string Derived::name() const{return "Derived";}
+
+class MoreDerived:public Derived{
+  public:
+    std::string name() const override;
+    std::string most_name() const override;
+};
+std::string MoreDerived::name() const{return "MoreDerived";}
+
+int main(){
+  Derived de;
+}
