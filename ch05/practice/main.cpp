7a380ad569dd07204aeab8587eece70ade33a810
yusuke <yuusuke29413@gmail.com>
2025-03-13 09:02:22 +0900

    ok

--- ch05/practice/main.cpp
+++ ch05/practice/main.cpp
@@ -2,39 +2,16 @@
 #include <string>
 #include <vector>
 #include <initializer_list>
-class A
-{
-    int *memberA;
-  public:
-    A();
-    A(const A& other); 
-    A(A&& other);
-    ~A();
 
-    void set(int memberA){
-      this->memberA = memberA;
-    }
-};
-A::A() : memberA{new int[100]} {
-  std::cout << "default constructor" << std::endl;
-}
-A::A(const A& other) : memberA(other.memberA){
-  std::cout << "copy constructor" << std::endl;
-}
-A::A(A&& other) : memberA(other.memberA){
-  other.memberA = nullptr;
-}
-
-A::~A(){
-  delete[] memberA;
+int myadd(int a, int b){
+  return a + b;
 }
 
 int main()
 {
-  A a;
-  a.set(42);
+  int (*func)(int, int) = &myadd;
+  std::cout << func(90, 18) << std::endl;
 
-  A b{a};
-  A c{std::move(a)};
-
+  int (&func_)(int, int) = myadd;
+  std::cout << func(49, 22) << std::endl;
 }
