d94b1f243ddcff50adae27a79ea9ec595f5c58cc
yusuke <yuusuke29413@gmail.com>
2025-03-16 07:46:19 +0900

    summary c5

--- ch05/practice/main.cpp
+++ ch05/practice/main.cpp
@@ -3,16 +3,19 @@
 #include <vector>
 #include <initializer_list>
 
-void clear(int& num){
-  std::cout << "reference: " << num << std::endl;
-}
+// void show(int v){
+//   std::cout << v << std::endl;
+// }
 
-void right(int&& num){
-  std::cout << "right: " << num << std::endl;
+void enumerate(int *array, int *array_end, void (*show)(int v)){
+  for (int i = 0; i < array_end - array; i++){
+    show(array[i]);
+  }
 }
 
 int main(){
-  int a = 100;
-  clear(a);
-  right(std::move(a));
+  int array[] = {1,2,3,5,7,11,13};
+
+  std::size_t length = sizeof(array) / sizeof(array[0]);
+  enumerate(array, array + length, [](int v){std::cout << v << std::endl;});
 }
