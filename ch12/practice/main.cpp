eb438caa371dd569015200998c8399af61bc4918
yusuke <yuusuke29413@gmail.com>
2025-04-14 22:26:30 +0900

    algorithm

--- ch12/practice/main.cpp
+++ ch12/practice/main.cpp
@@ -4,14 +4,25 @@
 #include <list>
 #include <tuple>
 #include <set>
+#include <algorithm>
+#include <iterator>
 
 int main()
 {
-  std::set<int> s{1, 1, 2, 2, 3, 3, 4, 4, 4, 5};
+  std::vector<int> v = {1, 2, 3, 3, 1, 2, 5, 3, 2, 1, 4, 50};
 
-  for (auto it = s.begin(); it != s.end(); it++)
-  {
-    std::cout << *it << " ";
-  }
-  std::cout << std::endl;
+  auto it = v.begin();
+
+  // int count = 0;
+  // for (auto num : v)
+  // {
+  //   if (*it == num)
+  //   {
+  //     count++;
+  //   }
+  // }
+
+  int count = std::count(v.begin(), v.end(), v.at(0));
+
+  std::cout << count << std::endl;
 }
